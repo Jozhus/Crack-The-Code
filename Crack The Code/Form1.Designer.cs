d156e5b580f283bb1e10d67018adcba785b957ab
Jozhus <Jozhus@users.noreply.github.com>
2019-09-27 23:49:08 -0700

    Initial

---
+++ Crack The Code/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Crack_The_Code
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button10 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnguess = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnexact = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnexists = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(150, 203);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(51, 51);
+            this.button9.TabIndex = 1;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(81, 203);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(51, 51);
+            this.button8.TabIndex = 2;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(150, 141);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(51, 51);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(81, 141);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(51, 51);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 203);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(51, 51);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(150, 78);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(51, 51);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 141);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(51, 51);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(81, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(51, 51);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(51, 51);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(3);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(18);
+            this.label1.Size = new System.Drawing.Size(51, 51);
+            this.label1.TabIndex = 11;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(81, 12);
+            this.label2.Margin = new System.Windows.Forms.Padding(3);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(18);
+            this.label2.Size = new System.Drawing.Size(51, 51);
+            this.label2.TabIndex = 11;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(150, 12);
+            this.label3.Margin = new System.Windows.Forms.Padding(3);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(18);
+            this.label3.Size = new System.Drawing.Size(51, 51);
+            this.label3.TabIndex = 11;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(218, 203);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(100, 51);
+            this.button10.TabIndex = 1;
+            this.button10.Text = "Reset";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnguess,
+            this.columnexact,
+            this.columnexists});
+            this.listView1.Location = new System.Drawing.Point(218, 12);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Scrollable = false;
+            this.listView1.Size = new System.Drawing.Size(99, 179);
+            this.listView1.TabIndex = 12;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnguess
+            // 
+            this.columnguess.Text = "Guess";
+            this.columnguess.Width = 45;
+            // 
+            // columnexact
+            // 
+            this.columnexact.Text = "O";
+            this.columnexact.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnexact.Width = 25;
+            // 
+            // columnexists
+            // 
+            this.columnexists.Text = "△";
+            this.columnexists.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnexists.Width = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 262);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Crack the Code";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnguess;
+        private System.Windows.Forms.ColumnHeader columnexact;
+        private System.Windows.Forms.ColumnHeader columnexists;
+
+    }
+}
+
---
+++ Crack The Code/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Crack_The_Code
+{
+    public partial class Form1 : Form
+    {
+        int round = 0;
+        int digit = 0;
+        int[] numbers;
+        Label[] digits;
+        static Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            digits = new Label[3] { label1, label2, label3 };
+            numbers = createNumber();
+            foreach (var button in Controls.OfType<Button>())
+            {
+                button.Click += button_Click;
+            }
+        }
+
+        int[] createNumber()
+        {
+            int[] newDigits = new int[3];
+            for (int i = 0; i < 3; ++i)
+            {
+                int newDigit;
+                do
+                {
+                    newDigit = random.Next(1, 10);
+                    Console.WriteLine(newDigit);
+                }
+                while (newDigits.Contains(newDigit));
+                newDigits[i] = newDigit;
+            }
+            return newDigits;
+        }
+
+        void check()
+        {
+            int exacts = 0;
+            int exists = 0;
+            for (int i = 0; i < 3; ++i)
+            {
+                if (numbers[i] == int.Parse(digits[i].Text)) exacts++;
+                else if (numbers.Contains(int.Parse(digits[i].Text))) exists++;
+            }
+            string[] arr = new string[3];
+            arr[0] = digits[0].Text + digits[1].Text + digits[2].Text;
+            arr[1] = exacts.ToString();
+            arr[2] = exists.ToString();
+            ListViewItem itm = new ListViewItem(arr);
+
+            itm.UseItemStyleForSubItems = false;
+
+            if (exacts > 0) itm.SubItems[1].ForeColor = System.Drawing.Color.Green;
+            if (exists > 0) itm.SubItems[2].ForeColor = System.Drawing.Color.Orange;
+
+            listView1.Items.Add(itm);
+
+
+            foreach (var button in Controls.OfType<Button>())
+            {
+                button.Enabled = true;
+            }
+            foreach (var label in digits)
+            {
+                label.Text = "";
+            }
+
+            if (exacts == 3)
+            {
+                MessageBox.Show("You win", "Winner", MessageBoxButtons.OK);
+                reset();
+            }
+            
+            if (round > 7)
+            {
+                MessageBox.Show("You lose", "Loser", MessageBoxButtons.OK);
+                reset();
+            }
+
+            digit = 0;
+            round++;
+        }
+
+        void reset()
+        {
+            round = 0;
+            digit = 0;
+            numbers = createNumber();
+
+            foreach (var button in Controls.OfType<Button>())
+            {
+                button.Enabled = true;
+            }
+            foreach (var label in digits)
+            {
+                label.Text = "";
+            }
+
+            listView1.Items.Clear();
+        }
+
+        void button_Click(object sender, System.EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (button.Text.Equals("Reset"))
+            {
+                reset();
+            }
+            else
+            {
+                digits[digit].Text = button.Text;
+                button.Enabled = false;
+                button10.Focus();
+                digit++;
+                if (digit > 2) check();
+            }
+        }
+    }
+}
